1ca3522d39cfa174733c0bcbfd6fb5f67e4a5d96
speedlight <tao.ceel@gmail.com>
2023-12-09 18:44:12 -0500

    Adding sample cloud functions

---
+++ cloud-functions/code/python-example-cf/main.py
@@ -0,0 +1,16 @@
+from flask import escape
+
+import functions_framework
+
+@functions_framework.http
+def main(request):
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and "name" in request_json:
+        name = request_json["name"]
+    elif request_args and "name" in request_args:
+        name = request_args["name"]
+    else:
+        name = "World"
+    return f"Hello {escape(name)}!"
